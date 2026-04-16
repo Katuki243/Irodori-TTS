47b64c978fa1f7743338de47289ec39936cc6db6
Katuki243 <126213888+Katuki243@users.noreply.github.com>
2026-04-16 11:14:07 +0900

    Delete run_test/reina.ps1

--- run_test/reina.ps1
+++
@@ -1,5 +0,0 @@
-uv run python infer.py `
-    --hf-checkpoint Aratako/Irodori-TTS-500M-v2 `
-    --text "私の名前はほしわたりナユタ、君のアシスタント、よろしく。" `
-    --ref-wav ref/shirozawa.wav `
-    --output-wav outputs/shirozawa1.wav
